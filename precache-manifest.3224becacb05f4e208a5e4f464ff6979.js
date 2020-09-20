270dc3cedbe6425d6008acbde7d5e8ae1fdaa179
ChaitanyaDave <chaitanya.dave@ucdconnect.ie>
2020-09-20 22:15:49 +0100

    Deploying my portfolio version 1.0

---
+++ precache-manifest.3224becacb05f4e208a5e4f464ff6979.js
@@ -0,0 +1,178 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2f58610f5ab4eb5ffda4af734f24669b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "be918cb6a9f3e162d362",
+    "url": "/static/css/3.9998556b.chunk.css"
+  },
+  {
+    "revision": "d268501ecbf354e07283",
+    "url": "/static/css/4.bcc701bd.chunk.css"
+  },
+  {
+    "revision": "1291f6329f402a16009d",
+    "url": "/static/css/main.94bcbffe.chunk.css"
+  },
+  {
+    "revision": "1751d468f1fc7def266e",
+    "url": "/static/js/2.4e4df02a.chunk.js"
+  },
+  {
+    "revision": "71772b085033e92f89cdfb3071333458",
+    "url": "/static/js/2.4e4df02a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "be918cb6a9f3e162d362",
+    "url": "/static/js/3.ac8f5e97.chunk.js"
+  },
+  {
+    "revision": "d268501ecbf354e07283",
+    "url": "/static/js/4.1c921f2b.chunk.js"
+  },
+  {
+    "revision": "1291f6329f402a16009d",
+    "url": "/static/js/main.79b32744.chunk.js"
+  },
+  {
+    "revision": "60c07a582ac6758cbeff",
+    "url": "/static/js/runtime-main.f0da5244.js"
+  },
+  {
+    "revision": "9332b68933af96774933d594113a6ac5",
+    "url": "/static/media/Accenture_Award.9332b689.jpg"
+  },
+  {
+    "revision": "21f233e19402cc4a66866a7f31191f0d",
+    "url": "/static/media/Agustina.21f233e1.woff"
+  },
+  {
+    "revision": "5c3644de6f5a3c918cc2404150499320",
+    "url": "/static/media/Bloomberg_Market_Concept.5c3644de.jpg"
+  },
+  {
+    "revision": "9d2e30635750978ef613254b2e83ae45",
+    "url": "/static/media/Excel Skills.9d2e3063.jpg"
+  },
+  {
+    "revision": "1c50abf70dd146b0eaa0b35e87a0fa1d",
+    "url": "/static/media/HackerRank SQL.1c50abf7.png"
+  },
+  {
+    "revision": "2bb943aa98bad5189746c7673a592052",
+    "url": "/static/media/Lean Six Sigma Black Belt.2bb943aa.jpg"
+  },
+  {
+    "revision": "ee6539921d713482b8ccd4d0d23961bb",
+    "url": "/static/media/Montserrat-Regular.ee653992.ttf"
+  },
+  {
+    "revision": "18a2f82174b0afa8295154fd350f5426",
+    "url": "/static/media/Power BI_certificate.18a2f821.jpg"
+  },
+  {
+    "revision": "c59720d63dc3595a12e1c7e641a1bfa5",
+    "url": "/static/media/airbnbLogo.c59720d6.png"
+  },
+  {
+    "revision": "47a7a717679b640ceb1b05d38c480a61",
+    "url": "/static/media/codeInLogo.47a7a717.webp"
+  },
+  {
+    "revision": "68e841388612be0c5143e2932fb77d4f",
+    "url": "/static/media/contactMail.68e84138.webp"
+  },
+  {
+    "revision": "59389695208d7454c6607bed51ed4316",
+    "url": "/static/media/developerActivity.59389695.svg"
+  },
+  {
+    "revision": "8ddf76a14a2e3ad3ba62b46d49a75a74",
+    "url": "/static/media/facebookLogo.8ddf76a1.png"
+  },
+  {
+    "revision": "c24ba79111a90758a3a472a906ab2901",
+    "url": "/static/media/googleAssistant.c24ba791.svg"
+  },
+  {
+    "revision": "e79c639294c805688be731921368c8f8",
+    "url": "/static/media/googleAssistantLogo.e79c6392.webp"
+  },
+  {
+    "revision": "c350d7996596e9909fe72c0938cc9ee5",
+    "url": "/static/media/iiitk_logo.c350d799.png"
+  },
+  {
+    "revision": "b05ae5b575a7e1125610944dc152c35f",
+    "url": "/static/media/jsFramework.b05ae5b5.svg"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/static/media/logo.25bf045c.svg"
+  },
+  {
+    "revision": "52be31441c8cf8a8a16bd7a228d428bf",
+    "url": "/static/media/manOnTable.52be3144.svg"
+  },
+  {
+    "revision": "3d6a2ad56bc3403c5cfcc3efe09b741b",
+    "url": "/static/media/microsoft_logo.3d6a2ad5.png"
+  },
+  {
+    "revision": "829ad0a1deef4d7295adfecdf12df2be",
+    "url": "/static/media/nextuLogo.829ad0a1.webp"
+  },
+  {
+    "revision": "6d65d682f3f86bd90bfc94602cdacb6b",
+    "url": "/static/media/nss_logo.6d65d682.jpg"
+  },
+  {
+    "revision": "c1a95f7335cbfb730b937e19e8830a53",
+    "url": "/static/media/programmer.c1a95f73.svg"
+  },
+  {
+    "revision": "1a60e8a9e2c2d6a539c7692f27fe6790",
+    "url": "/static/media/pwa.1a60e8a9.webp"
+  },
+  {
+    "revision": "ee62cb58630f29a6e6201b49d4c03728",
+    "url": "/static/media/pwaLogo.ee62cb58.webp"
+  },
+  {
+    "revision": "4b2938b9f5ea2f1ddbd45dccaff628bc",
+    "url": "/static/media/quoraLogo.4b2938b9.png"
+  },
+  {
+    "revision": "25018d29d5ab115bc51c3e9a2d92f90f",
+    "url": "/static/media/saayaHealthLogo.25018d29.webp"
+  },
+  {
+    "revision": "3440939881da8339d1597e8d77cdf850",
+    "url": "/static/media/skill.34409398.svg"
+  },
+  {
+    "revision": "d4b87b173d0f422fc9c8343373ff547a",
+    "url": "/static/media/smurfit_logo.d4b87b17.png"
+  },
+  {
+    "revision": "c587acb37799c798101a1b6b1108bd11",
+    "url": "/static/media/suit.c587acb3.jpg"
+  },
+  {
+    "revision": "39eff4a91b01e00a32cb78f3797eec88",
+    "url": "/static/media/talksCardBack.39eff4a9.svg"
+  },
+  {
+    "revision": "7f0feeedda08cf7d68593454bc2e82b5",
+    "url": "/static/media/talksCardBackAlt.7f0feeed.svg"
+  },
+  {
+    "revision": "be5c6053ecd4903a9e11d8a9ccf4b966",
+    "url": "/static/media/tcet_logo.be5c6053.jpg"
+  },
+  {
+    "revision": "77f34bf56a34db115c9bec32125ed7ff",
+    "url": "/static/media/ucd_logo.77f34bf5.png"
+  }
+]);
